c3088237eeb24f28b23ad8d3eafae1ebe083e8a3
Andrew Vitale <vitale232@gmail.com>
2020-03-10 07:25:30 -0400

    Change conda env name to osm_highways

--- download_and_extract_osm_schedule.bat
+++ download_and_extract_osm_schedule.bat
@@ -1,4 +1,4 @@
-call D:\Program_Files\miniconda3\condabin\activate.bat osmdiff_viewer
+call D:\Program_Files\miniconda3\condabin\activate.bat osm_highways
 python D:\OpenStreetMap\ExtractNewYork\download_and_extract_osm.py
 timeout 5
 call conda deactivate
